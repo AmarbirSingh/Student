60b243b702d98720346f0d158dd5f336f30ec829
AmarbirSingh <Amarbir303@gmail.com>
2019-05-21 13:09:18 -0400

    Commit 3

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/PartTime.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.sheridancollege.week1.softwarefundamentals;
+
+
+public class PartTime {
+
+    private String Name;
+
+    /**
+     * Get the value of Name
+     *
+     * @return the value of Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * Set the value of Name
+     *
+     * @param Name new value of Name
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+}
